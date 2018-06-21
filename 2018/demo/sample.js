020a189d3ef7ddd10360131c46ffc3c82d38578c
Han <han.yoong@daimler.com>
2018-06-21 08:39:30 +0800

    Latest for 2018

--- 2018/demo/sample.js
+++ 2018/demo/sample.js
@@ -3,13 +3,6 @@ var ReactNative = require('react-native');
 var {Text} = ReactNative;
 
 var SimpleApp = React.createClass({
-    render() {
-        return (
-            <View>
-                <Text>Hello</Text>
-            </View>
-        )
-    }
 });
 
 ReactNative.AppRegistry.registerComponent('SimpleApp', () => SimpleApp);
