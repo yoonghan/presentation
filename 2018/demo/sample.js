b5440fce67b19c6e05d476f90e6ac422d0cf26a1
Han <han.yoong@daimler.com>
2018-06-21 08:23:59 +0800

    Latest for 2018

--- 2018/demo/sample.js
+++ 2018/demo/sample.js
@@ -1,35 +1,12 @@
-'use strict';
-
 var React = require('react');
 var ReactNative = require('react-native');
-var {
-    Text,
-    View,
-    TouchableOpacity,
-    NativeModules,
-    Dimensions,
-} = ReactNative;
-
-var width = Dimensions.get('window').width;
-var Module = require('@remobile/react-native-module');
+var {Text} = ReactNative;
 
 var SimpleApp = React.createClass({
-    goBack() {
-        Module.unload({text: 'I come back'});
-    },
     render() {
         return (
             <View style={{flex: 1, backgroundColor: 'transparent', width:width}}>
-                <View style={{flex:1,  backgroundColor: 'gray', justifyContent:'center', alignItems:'center'}}>
-                    <TouchableOpacity onPress={this.goBack}>
-                        <Text style={{color:'white', fontSize:25}}>Back</Text>
-                    </TouchableOpacity>
-                </View>
-                <View style={{flex:1,  backgroundColor: 'blue'}}>
-                    <Text>
-                        {JSON.stringify(this.props)}
-                    </Text>
-                </View>
+                <Text>Hello</Text>
             </View>
         )
     }
