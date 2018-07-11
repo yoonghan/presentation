a1df81a4b92f682a32b1ff670ff14430782114f2
Han <han.yoong@daimler.com>
2018-07-11 13:56:51 +0800

    For presentation purpose

---
+++ 2018/demo/google-analytics.js
@@ -0,0 +1,2 @@
+alert("This javascript has just been comprimised as it is hosted externally.\n How would you secure it?");
+document.body.innerHTML="<h1 style='padding:50px'>Task 2, this page is not visible.</h1>"
